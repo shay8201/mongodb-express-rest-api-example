4cd1a2884b503782033c14cc9ea102482d127082
shay8201 <zshay8201@gmail.com>
2024-08-29 12:55:04 +0300

    add k8s files

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = process.env.ATLAS_URI || "";
+const connectionString = process.env.CONN_STR || "";
 
 const client = new MongoClient(connectionString);
 
