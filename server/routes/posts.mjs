b4be8cceca7f982cab37827ffac68856d48a4bc2
shay8201 <zshay8201@gmail.com>
2024-09-02 18:26:00 +0300

    uptade get method

--- server/routes/posts.mjs
+++ server/routes/posts.mjs
@@ -5,10 +5,19 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 // Get a list of 50 posts
+// router.get("/", async (req, res) => {
+//   let collection = await db.collection("posts");
+//   let results = await collection.find({})
+//     .limit(50)
+//     .toArray();
+
+//   res.send(results).status(200);
+// });
+
+// Get a list of all posts
 router.get("/", async (req, res) => {
   let collection = await db.collection("posts");
   let results = await collection.find({})
-    .limit(50)
     .toArray();
 
   res.send(results).status(200);
